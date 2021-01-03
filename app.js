90ff21a3f17fc6db6a1f918d8264d015aa917968
Ahmed Mansour <ahmedmmansour18@gmail.com>
2021-01-03 16:52:33 +0200

    first version

---
+++ app.js
@@ -0,0 +1,10 @@
+function imgSelected(img){
+    document.querySelector('.starbuksImg').src = img;
+  }
+  
+  
+  function channgCircleColor(color){
+    let circle = document.querySelector('.circle');
+    circle.style.background = color;
+  }
+  
